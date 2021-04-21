c5130408b9ca3b922b0f9dda7c439f270b9fc04a
Lagomoro <2669708373@qq.com>
2021-04-21 14:57:04 +0800

    Update Router

--- src/router/index.js
+++ src/router/index.js
@@ -7,13 +7,13 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+    /*{
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    },
+    },*/
     {
-      path: '/img',
+      path: '/',
       name: 'TestImgProcess',
       component: TestImgProcess
     }
